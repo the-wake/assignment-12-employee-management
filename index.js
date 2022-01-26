0ed6402feb639b71e0dd9bbad5c4c1f4efb38b76
Ben Martin <bmartin2009@gmail.com>
2022-01-25 18:54:33 -0600

    Starting functions

--- index.js
+++ index.js
@@ -10,32 +10,53 @@ const optionsArr = [
     },
 ];
 
+function optionSwitch(choice) {
+    switch (choice) {
+        case 'View All Departments':
+            functArr[0]('department');
+            break;
+        case 'View All Roles':
+            functArr[0]('role');
+            break;
+        case 'View All Employees':
+            functArr[0]('employee');
+            break;
+        case 'Add a Department':
+            functArr[1]('department');
+            break;
+        case 'Add a Role':
+            functArr[1]('role');
+            break;
+        case 'Add an Employee':
+            functArr[1]('employee');
+            break;
+        case 'Update an Employee Role':
+            functArr[2]();
+            break;
+        case 'Terminate Application':
+            functArr[3]();
+            break;
+    }
+};
+
 const functArr = [
-    function viewDepts(table) {
-        console.log('Viewing Departments.');
+    function viewEl(choice) {
+        console.log(`Viewing table "${choice}"`);
+        db.query(`SELECT * FROM ??`, choice, (err, results) => {
+            if (err) {
+                console.error(err);
+            }
+            console.log(results);
+        })
+    },
+    function addEl(choice) {
         inquirer.prompt(
             {
                 type: 'input',
-                message: `What is the name of the ${table}?`,
+                message: `What is the name of the ${choice}?`,
                 name: 'newEntry',
             }
         );
-        db.query(` `)
-    },
-    function viewRoles() {
-        console.log('Viewing Roles.');
-    },
-    function viewEmployees() {
-        console.log('Viewing Employees.');
-    },
-    function addDept() {
-        console.log('Adding a Department.');
-    },    
-    function addRole() {
-        console.log('Adding a Role.');
-    },    
-    function addEmployee() {
-        console.log('Adding a Employee.');
     },    
     function updateEmp() {
         console.log('Updating an Employee Role.');
@@ -53,37 +74,6 @@ const db = mysql.createConnection(
         database: 'a12_Staff'
     },
 );
-
-
-
-function optionSwitch(choice) {
-    switch (choice) {
-        case 'View All Departments':
-            functArr[0]('Departments');
-            break;
-        case 'View All Roles':
-            functArr[1]();
-            break;
-        case 'View All Employees':
-            functArr[2]();
-            break;
-        case 'Add a Department':
-            functArr[3]();
-            break;
-        case 'Add a Role':
-            functArr[4]();
-            break;
-        case 'Add an Employee':
-            functArr[5]();
-            break;
-        case 'Update an Employee Role':
-            functArr[6]();
-            break;
-        case 'Terminate Application':
-            functArr[7]();
-            break;
-    }
-};
 
 function init() {
     inquirer.prompt(optionsArr)
